3e8fdc725616ae8b6094e135c9c64eece0d354a0
marha456 <martin.danek456@gmail.com>
2015-12-10 16:11:35 +0100

    Update user.php

--- Sources/_controllers/user.php
+++ Sources/_controllers/user.php
@@ -1,10 +1,15 @@
 <?php
 
-    	 
+    	 /**
+    	  * Classa používateľa
+    	  */
  class User{
 
 	 public $userData = array();
 	
+	/**
+	 * Konštruktor triedy USER , konštruktor iba načíta súbory podtried
+	 */
 	public function __construct()
 	{
 
@@ -15,27 +20,118 @@
 
     	 
 	}
+	/**
+	 * Funkcia zabezpečuje zobrazenie POP-UP okna pre prihlásenie
+	 */
+
+	public function printLogPop()
+	{
+
+		echo User_v::showLogPop();
+	}
+
+	/**
+	 * funckia kontroluje či prebehlo odhlásenie a následne 
+	 * zruší SESSION premennú obsahujúcu ID užívateľa
+	 * 
+	 */
+	public function checkIfLogoff(){
+		
+		
+
+	if(isset($_POST["submitLogoff"])){
+				
+				unset($_SESSION["userId"]);
+
+				}
+		
+	}
+	/**
+	 * funckia kontroluje či prebehlo prihlásenie a následne 
+	 * nastaví SESSION premennú obsahujúcu ID užívateľa
+	 * 
+	 */ 
+	public function checkIfLogin()
+	{
+		if(isset($_POST["submitLog"])){
+				if(isset($_POST["login"]) && isset($_POST["pass"]))
+				{
+				
+					$this->login($_POST["login"],$_POST["pass"]);
+
+				}
+		}
+	}
+
+///funckia ,zisti ,ci prebehla editacia profilu a nasledne vola funckia ktore zmenu vykona/
+///
+
+
 	
+	/**
+	 * funkcia zabezpečí login na základe emailu a hesla a naplnenie údajov z databázy
+	 * @param  [type] $login    [email používateľa]
+	 * @param  [type] $password [heslo]
+	 * @return [type]           [údaje používateľa získane z DB]
+	 */
 	 public function login($login,$password)
 	 {
 
 	 	
-	 	$userData=User_m::getUserDataByLogin($login,$password);
+	 	$this->userData=User_m::getUserDataByLogin($login,$password);
+	 	$_SESSION["userId"]=$this->userData["id"];
+
 	 	
-	 /*	if(User_m!=null){               // nastavenie vlastnosti modulu z databazy ak je to existujuci modul
-	      $this->userData = User_m::getModuleContainer($id); }*/
-	      
 	    
-	    return $userData;
+	    return $this->userData;
 	 }
 
-		public function printRegForm($id){
+	 /**
+	  * funckia naplní údaje o používateľévi z DB na základe ID
+	  * @param  [type] $id [ID používateľa]
+	  */
+	 public function fillUserDataById($id)
+	 {
+	 	$this->userData=User_m::getUserDataById($id);
 
-		 	User_v::showRegForm($userData);
+	 }
+	 public function fillUserDatabySession()
+	 {
+	 	if(isset($_SESSION["userId"])){
+	 	$this->userData=User_m::getUserDataById($_SESSION["userId"]);
+	 	}
+	 	else {echo "neni prihlaseny user";}
+	 }
 
-		 }
+	
 
+	 /**
+	  * funckia zobrazí formulár na registráciu
+	  */
 
+	 public function printUserSectionReg()
+	 {
+	 	return User_v::showRegForm();
+	 }
+
+	 /**
+	  * funkcia zobrazí sekciu na správu používateľského konta používateľa
+	  * @return [type] [description]
+	  */
+	 public function printUserSection()
+	 {
+	 	
+	 	return User_v::showUserSection($this->userData);
+
+	 }
+
+	 
+	
+
+		 /**
+		  * funcia na spracovanie registrácie po kontrole validity
+		  * 
+		  */
 	  	public function processRegistration($parameters){
 
 	 	User_m::checkValidReg($parameters);
@@ -43,32 +139,37 @@
 
 
 		 }
-
+/*
 		 public function printUpdateForm(){
 
 		 		User_v::showUpdateForm($userData);
-		 }
+		 }*/
 
+ 
 
-		 public function processUpdate($parameters)
-		 {
-
-		 	User_m::updateUserToDb($parameters);
-		 }
-
-
+		 /**
+		  * funkcia na zmazenie používateľa
+		  * @param  [type] $id [ID používateľa]
+		  * 
+		  */
 		 public static function delete($id){
 
 		 	User_m::deleteUserFromDb($id);
 		 	__destroy();
 		 }
 	
-
+		 /**
+		  * funckia zobrazí zoznam všetkých používateľov
+		  * 
+		  */
 		public function showUsersList()
 		{
 			User::getListOfUsers();
 		}
-
+		/**
+		 * funkcia zobrazí zoznam žiadostí o schválenie
+		 * 
+		 */
 		public function showApproveList()
 		{
 			User_m::getListOfApprovals();
