3f1a2eb875eaceb16da490330047bf7c875c7625
marha456 <martin.danek456@gmail.com>
2015-12-10 16:13:46 +0100

    Update User_m.php

--- Sources/_models/User_m.php
+++ Sources/_models/User_m.php
@@ -1,12 +1,33 @@
 <?php
 
-	class User_m{
+	class User_m extends User{
 
 public function __construct(){
 	require('../models/Db.php');
 
 
 }
+			/**
+			 * funkcia vykoná editáciu používateľa v DB
+			 * @param  [type] $param [zmenené údaje]
+			 * 
+			 */
+			public static function editUserToDb($param)
+			{
+				$result = Db::query("
+				     UPDATE user set email= ? ,first_name=? ,
+				     last_name=?,admin=?, reg_date=?,bio=?,password=? 
+				     where id=? ",
+				      array($param['login'],$param['firstName'],
+				      	$param['lastName'],
+				      	0,'2014-01-01',
+				      	
+				      	$param['bio'],
+				      $param['pass'],
+				      $param['id']))->fetch();
+				    return $result;
+
+			}
 
 			public static function getUserDataByLogin($mail,$pass)
 			{
@@ -19,6 +40,21 @@ public function __construct(){
 				    return $result;
 
 			}
+			public static function getUserDataById($id)
+			{
+
+					 $result = Db::query("
+				      SELECT *
+				      FROM user u
+				      WHERE u.id=?", 
+				      array($id))->fetch();
+				    return $result;
+
+			}
+			/**
+			 * funckia vráti všetkých používateľov v DB
+			 * @return [list] [zoznam používateľov]
+			 */
 			public static function getUsers()
 			{
 				$result = Db::query("
@@ -31,7 +67,12 @@ public function __construct(){
 
 			}
 		
-
+			/**
+			 * funckia kontroluje či sa v databáze nachádza používateľ so
+			 * zadaným emailom a heslom
+			 * @param  [type] $param [email a heslo]
+			 * @return [boolean]        [vrati true/false,podľa toho či nájde taký záznam]
+			 */
 			public static function checkValidReg($param){
 				$result = Db::query("
 				      SELECT *
@@ -45,16 +86,24 @@ public function __construct(){
 
 			}
 
+			/**
+			 * funckia pridá nový záznam používateľa do DB
+			 * @param  [type] $param [údaje]
+			 * 
+			 */
 			public static function adduserToDb($param){
-/*
+
 				$result = Db::query("
-				      INSER into
-				      user values (".$param[email]).""
-				     
-
-				     );*/
-
-
+				      INSERT into
+				      user (email,first_name,last_name,admin,
+				      	reg_date,bio,password)
+				      values (?,?,?,?,?,?,?)",
+				      array($param['login'],$param['firstName'],
+				      	$param['lastName'],
+				      	0,'2014-01-01',
+				      	$param['pass'],
+				      	$param['bio']));
+			
 
 			}
 
