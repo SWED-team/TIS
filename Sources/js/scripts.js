c59006bafbcd81bb89bfdad0266ae4185cebe176
Martin Krasňan <martin.krasnan@gmail.com>
2016-01-29 14:54:50 +0100

    danek oprava scriptu

--- Sources/js/scripts.js
+++ Sources/js/scripts.js
@@ -1,4 +1,18 @@
 
+        $.fn.modal.Constructor.prototype.enforceFocus = function() {
+          modal_this = this
+          $(document).on('focusin.modal', function (e) {
+            if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length 
+            && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select') 
+            && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+              modal_this.$element.focus()
+            }
+          })
+        };
+        
+
+
+
 function showModal(modalId, size, header, body) {
     var modalHtml = '\
     <div class="modal fade" id="'+ modalId + '" role="dialog">\
